3aa59c239afef3d58be9bb5ab655cf904df67ac5
AnneSo-Mani <AnneSo-Mani@130.190.30.243>
2016-04-26 09:58:49 +0200

    bla bla bla

--- ApplicationTestAchala/src/application/panels/panelsChat/PanelChat.java
+++ ApplicationTestAchala/src/application/panels/panelsChat/PanelChat.java
@@ -1,58 +1,47 @@
 package application.panels.panelsChat;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
+import javax.swing.AbstractListModel;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
-import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
 
-import application.utils.Commentaire;
 import application.utils.Message;
 
-import javax.swing.JTextPane;
-import javax.swing.JList;
-import javax.swing.AbstractListModel;
-
 public class PanelChat extends JPanel {
+	
 	private JTextField txtmessage;
-
-	// private List<Contact> ListContacts = new ArrayList<Contact>();
-	// private List<PanelCommentaire> PanelListContact = new
-	// ArrayList<PanelContact>();
-
-	//private List<Contact> ListContacts = new ArrayList<Contact>();
 	private List<Message> ListMessage = new ArrayList<Message>();
 	
 	/**
-	 * Create the panel.
-	 */
-
+	* Create the panel.
+	*/
 	public PanelChat(String nom, String prenom, List<Message> ListMessage) {
-		System.out.println(getPreferredSize());
 		setBackground(Color.LIGHT_GRAY);
 		setLayout(null);
-
-		/* Titre du Panel */
 		
 		/*Initialisation de la liste des messages*/
 		ListMessage.add(new Message("Luc_ortiz", "18/12/2016 15h12", "Salut =)"));
@@ -60,52 +49,46 @@ public class PanelChat extends JPanel {
 		ListMessage.add(new Message("Luc_ortiz", "18/12/2016 15h18", "comment vas tu ?!"));
 		ListMessage.add(new Message("Hugo_Vaillant", "18/12/2016 15h18", "Bien, Merci Luc !"));
 		
-		/*Titre du Panel*/
-		JLabel lblChat = new JLabel("CHAT");
+		
+		/**
+		 * CHAT
+		*/
+		
+		/*LABEL CHAT*/
+		JLabel lblChat = new JLabel("CHAT :");
 		lblChat.setFont(new Font("Tahoma", Font.BOLD, 21));
 		lblChat.setHorizontalAlignment(SwingConstants.CENTER);
-		lblChat.setBounds(238, 11, 84, 25);
+		lblChat.setBounds(110, 13, 84, 25);
 		add(lblChat);
-
-		/* Panel scrollable */
 		
-		/*Panel scrollable des Contacts*/
-		JLabel lblContacts = new JLabel("Contacts en ligne");
-		lblContacts.setHorizontalAlignment(SwingConstants.CENTER);
-		lblContacts.setFont(new Font("Tahoma", Font.BOLD, 21));
-		lblContacts.setBounds(533, 144, 275, 25);
-		add(lblContacts);
+		/*LABEL NAME TO CHAT*/
+		JLabel lblNametoChat = new JLabel("Veuillez selectionner un contact");
+		lblNametoChat.setFont(new Font("Tahoma", Font.PLAIN, 19));
+		lblNametoChat.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNametoChat.setBounds(200, 13, 318, 25);
+		add(lblNametoChat);
 		
-		/*label ip Server*/
-		JLabel lblipServer = new JLabel("Choix du serveur");
-		lblipServer.setHorizontalAlignment(SwingConstants.CENTER);
-		lblipServer.setFont(new Font("Tahoma", Font.BOLD, 21));
-		lblipServer.setBounds(545, 31, 265, 25);
-		add(lblipServer);
-		
-		/*Panel du chat*/
+		/*PANEL CHAT*/
 		JPanel panelChat = new JPanel();
-		panelChat.setBounds(1, 1, 459, 345);
+		panelChat.setBounds(10, 11, 459, 345);
 		panelChat.setPreferredSize(new Dimension(500, 3000));
 		add(panelChat);
 		
-		
-		/*Scroll du chat*/
+		/*SCROLL CHAT*/
 		JScrollPane scrollPaneChat = new JScrollPane(panelChat);
 		scrollPaneChat.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPaneChat.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPaneChat.setBounds(57, 47, 478, 201);
+		scrollPaneChat.setBounds(40, 47, 478, 201);
 		add(scrollPaneChat);
-		
-		
+
+		/*INITIALISATION MESSAGES*/
 		PanelMessage pmsg;
 		for (Message m : ListMessage) {
 			pmsg = new PanelMessage(m);
 			panelChat.add(pmsg);
 		}
 		
-
-		/* Text Field saisie message */
+		/*FIELD SAISIE MESSAGE */
 		txtmessage = new JTextField();
 		txtmessage.addMouseListener(new MouseAdapter() {
 			@Override
@@ -115,25 +98,24 @@ public class PanelChat extends JPanel {
 		});
 		txtmessage.setHorizontalAlignment(SwingConstants.CENTER);
 		txtmessage.setText("Saisissez votre message ici ...");
-		txtmessage.setBounds(36, 259, 286, 71);
+		txtmessage.setBounds(40, 259, 282, 71);
 		add(txtmessage);
 		txtmessage.setColumns(10);
-
-		/* bouton ajouter un fichier */
+		
+		/*BOUTON FICHIER*/
 		JButton btnFichier = new JButton("Fichier");
 		btnFichier.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				System.out.println("Ouverture d'une fenêtre de selection fichier");
 			}
 		});
-		btnFichier.setBounds(332, 272, 101, 45);
+		btnFichier.setBounds(338, 264, 84, 60);
 		add(btnFichier);
 
-		/* bouton envoyer un message */
+		/*BOUTON ENVOYER*/
 		JButton btnEnvoyer = new JButton("Envoyer");
 		btnEnvoyer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				/* TODO envoye(r) */
 				String txtDate = new SimpleDateFormat("dd MMMM yyyy", Locale.FRANCE).format(new Date());
 				Message m = new Message(nom+"_"+prenom, txtDate,txtmessage.getText());
 				ListMessage.add(m);
@@ -143,41 +125,40 @@ public class PanelChat extends JPanel {
 				validate();
 			}
 		});
-		btnEnvoyer.setBounds(443, 264, 115, 60);
+		btnEnvoyer.setBounds(434, 264, 84, 60);
 		add(btnEnvoyer);
-
-		/* bouton connecter */
 		
-		/*ComboBox des ip server*/
-		JComboBox comboBoxIpServer = new JComboBox();
-		comboBoxIpServer.setEditable(true);
-		comboBoxIpServer.setModel(new DefaultComboBoxModel(new String[] {"192.168.12.55", "127.0.0.1"})); //TODO getAllIp()
-		/*TODO remplir la combox avec une list d'ip*/
-		comboBoxIpServer.setBounds(568, 79, 132, 34);
-		add(comboBoxIpServer);
 		
-		/*bouton connecter*/
-		JButton btnConnecter = new JButton("Connecter");
-		btnConnecter.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				System.out.println("Connection au server ip :" + comboBoxIpServer.getSelectedItem().toString());
+		
+		/**
+		 * CONTACT
+		*/
+		/*LABEL Contacts*/
+		JLabel lblContact = new JLabel("Contacts en ligne");
+		lblContact.setHorizontalAlignment(SwingConstants.CENTER);
+		lblContact.setFont(new Font("Tahoma", Font.BOLD, 21));
+		lblContact.setBounds(570, 144, 265, 25);
+		add(lblContact);
+		
+		/*LISTE CONTACT*/
+		JList<Component> listContact = new JList();
+		listContact.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				if (arg0.getID() == 401){
+					System.out.println("touche entrée préssée");
+					String s = "" + listContact.getModel().getElementAt(listContact.getSelectedIndex());
+					lblNametoChat.setText(s);
+					//charger messages du contact(modifier une liste locale)
+				}
+						
 			}
+			
 		});
-		btnConnecter.setBounds(828, 79, 98, 34);
-		add(btnConnecter);
-
-		/* ComboBox des ip server */
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] { "192.168.12.55", "127.0.0.1" }));
-		/* TODO remplir la combox avec une list d'ip */
-		comboBox.setBounds(710, 79, 107, 34);
-		add(comboBox);
-		
-		JList list = new JList();
-		String[] values2 = new String[] {"Luc_Ortiz", "Hugo_Vaillant", "Clarck_robinson", "Audrey_Claude", "Aur\u00E9lien_Fernandez"};
-		list.setModel(new AbstractListModel() {
-			String[] values = new String[] {"Luc_Ortiz", "Hugo_Vaillant", "Clarck_robinson", "Audrey_Claude", "Aur\u00E9lien_Fernandez"},
-			values2 = values;
+		listContact.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		listContact.setModel(new AbstractListModel() {
+			Object[] values = new Object[] {"Luc_Ortiz", "Hugo_Vaillant", "Clarck_robinson", "Audrey_Claude", "Aur\u00E9lien_Fernandez"};
+			//list d'objets
 			public int getSize() {
 				return values.length;
 			}
@@ -185,12 +166,40 @@ public class PanelChat extends JPanel {
 				return values[index];
 			}
 		});
-		list.setBounds(570, 180, 265, 150);
-		add(list);
-		System.out.println("Numéro du contact selectionné" +list.getSelectedIndex());
+		listContact.setBounds(568, 180, 265, 150);
+		add(listContact);
 		
+		
+		/**
+		 * SERVER
+		*/
+		/*LABEL Server*/
+		JLabel lblipServer = new JLabel("Choix du serveur");
+		lblipServer.setHorizontalAlignment(SwingConstants.CENTER);
+		lblipServer.setFont(new Font("Tahoma", Font.BOLD, 21));
+		lblipServer.setBounds(570, 31, 260, 25);
+		add(lblipServer);
 
-
+		/*COMBOX SERVER*/
+		JComboBox comboxIpServer = new JComboBox();
+		comboxIpServer.setEditable(true);
+		comboxIpServer.setModel(new DefaultComboBoxModel(new Object[] {"192.168.12.55", "127.0.0.1"})); // prend une liste d'objets
+		comboxIpServer.setBounds(568, 79, 154, 34);
+		add(comboxIpServer);
+		
+		
+		/*BOUTON CONNECTER*/		
+		JButton btnConnecter = new JButton("Connecter");
+		btnConnecter.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Connection au server ip :" + comboxIpServer.getSelectedItem().toString());
+				//listServeurip.add(comboxIpServer.getModel().getSelectedItem());
+				//seConnecter(comboxIpServer.getModel().getSelectedItem());
+			}
+		});
+		btnConnecter.setBounds(732, 79, 98, 34);
+		add(btnConnecter);
+		
 	}
 	
 }
